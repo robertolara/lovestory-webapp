9840521023a68335a781d454845d7a8132060c4b
robertolara <roberto.laraesp@gmail.com>
2016-11-05 20:34:18 -0600

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,40 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require modernizr.custom
+//= require jquery.min
+//= require jquery-ui.min
+//= require bootstrap.min
+//= require jquery.easing.min
+//= require jquery.sticky
+//= require jquery.parallax
+//= require masonry.pkgd.min
+//= require annyang.min
+//= require wow.min
+//= require waypoints.min
+//= require smoothscrolling
+//= require custom
+//= require fss
+//= require geo
+//= require jquery.countTo
+//= require jquery.mixitup.min
+//= require imagesloaded.pkgd.min
+//= require owl.carousel.min
+//= require jquery.magnific-popup.min
+//= require slide-fade-content
+//= require twitter/jquery.tweet
+//= require prefixfree
+//= require countup
+//= require modernizr.hover
